88f53bdcedab63c68998172cafef93164917ad49
Huann Almeida <huannvictor@gmail.com>
2024-02-06 22:49:12 -0300

    config: adding Pylint to Project

--- exemple.py
+++ exemple.py
@@ -1,19 +1,2 @@
-'''
-    docstring (documentação) de Modulo
-
-    Este documento tem como objetivo anotar e explicar alumas práticas do pylint:
-    - snake_case: usado em Funções, Variáveis e Métodos
-    - PascalCase: usado em Classes
-'''
-
 def greeting():
-    '''
-        docstring de função
-    '''
-    print('hello')
-
-def greet_snake_case():
-    '''
-        docstring de função
-    '''
     print('howdy')
